4e7e3c090eac7764fc20ca57f808eae701256a3d
Husain Bhagat <hussainbhagat52@gmail.com>
2024-10-12 19:04:32 +0530

    Feat: Doen FE setup and added hono

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import SignUpCard from "@/app/features/auth/components/sign-up-card"
+
+const SignUpPage = () => {
+    return ( 
+        <SignUpCard/>
+     );
+}
+ 
+export default SignUpPage;
